3307786b3aa1e4d7447ae80fcd4860bbb285c4fe
Jonas Eklöf <jonas.eklof@svt.se>
2015-04-22 17:03:45 +0200

    initial

---
+++ mickesmamma.py
@@ -0,0 +1,18 @@
+import yaml
+from flask import Flask, Response, request
+from flask import render_template
+from random import randint
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def start():
+    terms = yaml.load(open('mickesmamma.yml').read())['terms']
+    i = randint(0,2)
+    print i
+    return '<h1>%s</h1>' % terms[i]
+
+
+if __name__ == '__main__':
+    app.run()
